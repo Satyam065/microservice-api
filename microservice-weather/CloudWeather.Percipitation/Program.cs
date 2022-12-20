bb344a27f676a122c5150d43d0123154d6ea7014
Satyam065 <sonisatyam065@gmail.com>
2022-12-20 17:29:10 +0530

    add initial desp

--- microservice-weather/CloudWeather.Percipitation/Program.cs
+++ microservice-weather/CloudWeather.Percipitation/Program.cs
@@ -2,22 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.MapGet("/observation/{zip}", (string zip) =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+    return Results.Ok(zip);
+});
 app.Run();
